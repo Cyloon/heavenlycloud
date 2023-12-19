fd1c8a20d574ee78e1345647701596ef26c72c62
Cyloon <cyloon@gmail.com>
2023-12-19 14:51:19 +0100

    First react push after completed tutorial 1

---
+++ Reactor/week 1/website assessment/my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+import Gallery from "./Gallery.jsx";
+
+function App() {
+  
+  return(
+    <>
+      <Header />
+      <Gallery />
+      <Food />
+      <Footer />
+    </>
+  );
+}
+
+export default App
